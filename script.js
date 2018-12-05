14e6c9ea1224b39fb0fa8852e8c97c1d81a6f42b
danielp2003 <43018319+danielp2003@users.noreply.github.com>
2018-12-05 14:47:59 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#button1").click(function(){
+ $(".img1").slideToggle();
+});
+$("#button2").click(function(){
+ $(".img2").fadeToggle();
+});
+$("#button3").click(function(){
+ $(".img3").toggle();
+});
